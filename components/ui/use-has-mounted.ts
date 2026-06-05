b11a661b37daef7f2a46c061c9b14d1339a90030
tsaiggo <ttsaiggo@gmail.com>
2026-06-06 01:21:15 +0800

    Add mounted-state hydration gate
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

---
+++ components/ui/use-has-mounted.ts
@@ -0,0 +1,19 @@
+'use client';
+
+import { useSyncExternalStore } from 'react';
+
+function subscribe(): () => void {
+  return () => {};
+}
+
+function getClientSnapshot(): boolean {
+  return true;
+}
+
+function getServerSnapshot(): boolean {
+  return false;
+}
+
+export function useHasMounted(): boolean {
+  return useSyncExternalStore(subscribe, getClientSnapshot, getServerSnapshot);
+}
